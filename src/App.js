45af5abbc894435da61c0b7929f45162f9e7da11
dyh135ru <69703777+dyh135ru@users.noreply.github.com>
2020-09-22 10:33:12 -0600

    Version 2 guardar, editar y eliminar mejorado
    Se mejoro las funciones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, Input, Col, Row, Form } from "antd";
+import { Button, Input, Col, Row } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 import Name from "./components/Name";
 import Age from "./components/Age";
 import List from "./components/List";
@@ -9,21 +10,23 @@ import "./App.css";
 function App() {
   const [names, setNames] = useState([]);
   const [name, setName] = useState("");
-  const [enteredText, setEnteredText] = useState("");
-  const [form] = Form.useForm();
-  //const ages = ["21", "20", "15"];
-  const contentStyle = {
-    height: "160px",
-    color: "#fff",
-    lineHeight: "160px",
-    textAlign: "center",
-    background: "#364d79",
-  };
+  const [index, setIndex] = useState(null);
+
   function addName() {
-    if (name !== "" && names.indexOf(name) === -1) {
+    /* if (name !== "" && names.indexOf(name) === -1) {
       const newNames = [name, ...names];
       setNames(newNames);
-      form.resetFields();
+      setName("");
+    } */
+    if (index === null) {
+      const newNames = [name, ...names];
+      setNames(newNames);
+      setName("");
+    } else {
+      names[index] = name;
+      setNames([...names]);
+      setIndex(null);
+      setName("");
     }
   }
   function removeName(name) {
@@ -32,42 +35,36 @@ function App() {
     setNames(newNames); //El arreglo se vuelve a guardar sin el elemento filtrado
   }
 
-  function editName(nombre) {
-    const indice = names.indexOf(nombre);
-
-    if (name !== "" && names.indexOf(name) === -1) {
-      names[indice] = name;
-      form.resetFields();
-    }
+  function editName(name) {
+    //Index 0,1 ['Ernesto' , 'Cedano']
+    const index = names.indexOf(name);
+    //Guardar index = en el estadp
+    setIndex(index);
+    //names[index] names[1]
+    setName(names[index]);
   }
 
   function onChange(a, b, c) {
     console.log(a, b, c);
   }
-
   return (
     <div className="App">
       <Row justify="center" gutter={10}>
-        <Form form={form} onFinish={addName}>
-          <Col>
-            <Form.Item name="name">
-              <Input
-                onChange={(event) => {
-                  setName(event.target.value);
-                }}
-                placeholder="Ingrese un nombre"
-                value={enteredText}
-              />
-            </Form.Item>
-          </Col>
-          <Col>
-            <Button onClick={() => addName()} type="primary">
-              Agregar
-            </Button>
-          </Col>
-        </Form>
+        <Col>
+          <Input
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            placeholder="Ingrese un nombre"
+          />
+        </Col>
+        <Col>
+          <Button onClick={() => addName()} type="primary">
+            Agregar
+          </Button>
+        </Col>
       </Row>
-
       <List data={names} removeName={removeName} editName={editName} />
       {/* {names.map((name) => (
         <Name value={name} removeName={removeName} />
